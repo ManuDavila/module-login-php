d94c00e2c2ed14b69bbcc5871f6c1dcf890c1ee8
ManuDavila <manudg_1@msn.com>
2013-08-14 04:07:19 +0200

    Module Login PHP

--- login/login/Register.php
+++ login/login/Register.php
@@ -100,11 +100,11 @@ if (copy("http://www.gravatar.com/avatar/" . $mail_hash . "?s=" . $size_avatar .
 	{
 	unlink($avatar);
 	}
-}
-	else
+  	else
 	{
 	   $avatar = "avatars/gravatar.jpg";
 	}
+}
 	
 $query = "INSERT INTO users(nick, email, password, avatar, confirm, reset) VALUES('$nick', '$email', '".md5($password)."', '$avatar', '$confirm', '$reset')";
 $result = $connection -> query($query);
